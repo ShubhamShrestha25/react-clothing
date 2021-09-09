f1b70d43e864f3cc5877e63322e0f3ab98a79aa0
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-09-09 20:11:51 +0545

    filter added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <ProductList />
     </div>
   );
 }
