35f3f48cb47a4ef8a285c41ce8f7060d595d965a
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-09-10 21:33:53 +0545

    Single product page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
+import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
-      <ProductList />
+      <Product/>
     </div>
   );
 }
