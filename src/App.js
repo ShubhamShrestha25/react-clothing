f42297d2604e19320de51a29c09eee7fd5608362
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-09-13 19:19:57 +0545

    login and register page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <Product/>
+      <Login />
     </div>
   );
 }
