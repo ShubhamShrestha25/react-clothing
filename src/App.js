d5f83b9b475de27906f77630038e71655c4146e6
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-09-15 14:28:18 +0545

    cart page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Cart />
     </div>
   );
 }
