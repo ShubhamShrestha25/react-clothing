171e08eabac951c77ef37d751974065c7c528739
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-09-25 10:11:55 +0545

    api

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "https://i.ibb.co/XsdmR2c/1.png",
+    img: "https://i.ibb.co/cXFnLLV/3.png",
     title: "SUMMER SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "f5fafd",
