3d65a0b993e614c84743876e996d5d5d6f8dd1ae
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-09-27 18:32:13 +0545

    Login and Register

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ mongoose
   });
 
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
