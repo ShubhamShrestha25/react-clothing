d77ad27b09890ccfb262f57a7106bac8e790d0ad
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-09-28 20:09:27 +0545

    User route complete

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -43,7 +43,7 @@ router.post("/login", async (req, res) => {
         isAdmin: user.isAdmin,
       },
       process.env.JWT_SEC,
-      { expiresIn: "1d" }
+      { expiresIn: "3d" }
     );
 
     const { password, ...others } = user._doc;
