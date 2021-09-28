7b90e10dee4aeb45755e7fe63889e6e279d417e2
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-09-28 10:46:11 +0545

    Update user

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,3 +1,29 @@
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuth } = require("./verifyToken");
+
 const router = require("express").Router();
+
+//UPDATE
+router.put("/:id", verifyTokenAndAuth, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
